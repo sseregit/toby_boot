cd08d7534b879887c80ddde9e04039a381028d23
Macmini <sseregit@knowwheresoft.com>
2024-10-25 17:31:24 +0900

    Hello 컨트롤러 매핑과 바인딩

--- src/main/java/toby/boot/helloboot/HelloController.java
+++ src/main/java/toby/boot/helloboot/HelloController.java
@@ -1,12 +1,7 @@
 package toby.boot.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 class HelloController {
 
-    @GetMapping("/hello")
     String hello(String name) {
         return "Hello " + name;
     }
--- src/main/java/toby/boot/helloboot/HellobootApplication.java
+++ src/main/java/toby/boot/helloboot/HellobootApplication.java
@@ -18,23 +18,29 @@ class HellobootApplication {
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @java.lang.Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello " + name);
-                    } else if (req.getRequestURI().equals("/user")) {
-                        resp.setStatus(HttpStatus.UNAUTHORIZED.value());
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+            HelloController helloController = new HelloController();
+
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
+                        @Override
+                        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                            // 인증, 보안, 다국어, 공통 기능
+                            if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                                String name = req.getParameter("name");
+
+                                String ret = helloController.hello(name);
+
+                                resp.setStatus(HttpStatus.OK.value());
+                                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                                resp.getWriter().println(ret);
+                            } else if (req.getRequestURI().equals("/user")) {
+                                resp.setStatus(HttpStatus.UNAUTHORIZED.value());
+                            } else {
+                                resp.setStatus(HttpStatus.NOT_FOUND.value());
+                            }
+                        }
                     }
-                }
-            }).addMapping("/*");
+            ).addMapping("/*");
         });
         webServer.start();
     }
