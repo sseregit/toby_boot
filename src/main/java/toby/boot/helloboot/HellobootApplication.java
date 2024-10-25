790fd9a75b287c90d8168be0ec1ccd361b397c49
장연식 <sseregit@knowwheresoft.com>
2024-10-25 14:03:39 +0900

    프로젝트 생성

---
+++ src/main/java/toby/boot/helloboot/HellobootApplication.java
@@ -0,0 +1,13 @@
+package toby.boot.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellobootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toby/boot/helloboot/HellobootApplicationTests.java
@@ -0,0 +1,13 @@
+package toby.boot.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
