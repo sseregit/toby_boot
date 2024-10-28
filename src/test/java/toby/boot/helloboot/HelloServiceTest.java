f7285e9446ba500f5e0c190195c05e627b902c6a
Macmini <sseregit@knowwheresoft.com>
2024-10-28 14:46:50 +0900

    메타 애노테이션과 합성 애노테이션

--- src/test/java/toby/boot/helloboot/HelloServiceTest.java
+++ src/test/java/toby/boot/helloboot/HelloServiceTest.java
@@ -3,7 +3,26 @@ package toby.boot.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test
+@interface UnitTest {
+
+}
 
 class HelloServiceTest {
 
