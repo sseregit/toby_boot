de5b19ec2ee00e0a9071297ce37609844c223d00
sseregit <sseregit@naver.com>
2024-10-29 05:29:42 +0900

    @Conditional과 Condition

--- src/main/java/toby/boot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/toby/boot/config/autoconfig/JettyWebServerConfig.java
@@ -2,11 +2,12 @@ package toby.boot.config.autoconfig;
 
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import toby.boot.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 class JettyWebServerConfig {
 
     @Bean("jettyWebServerFactory")
@@ -14,4 +15,10 @@ class JettyWebServerConfig {
         return new JettyServletWebServerFactory();
     }
 
+    static class JettyCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
 }
--- src/main/java/toby/boot/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/toby/boot/config/autoconfig/TomcatWebServerConfig.java
@@ -3,9 +3,14 @@ package toby.boot.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import toby.boot.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
@@ -13,4 +18,10 @@ class TomcatWebServerConfig {
         return new TomcatServletWebServerFactory();
     }
 
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
+    }
 }
